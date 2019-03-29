b1c9f1e958f273b97840246d68f4c302458481d9
Sam <sam.legrand@student.uantwerpen.be>
2019-03-29 12:35:53 +0100

    Added debugging tools

---
+++ Utils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sam on 29/03/19.
+//
+
+#include "Utils.h"
---
+++ Utils.h
@@ -0,0 +1,16 @@
+//
+// Created by sam on 29/03/19.
+//
+
+#ifndef GAMEBOYEMULATOR_UTILS_H
+#define GAMEBOYEMULATOR_UTILS_H
+
+
+
+class Utils {
+
+};
+
+
+
+#endif //GAMEBOYEMULATOR_UTILS_H
